6968efb503a012cef50fb1be5c33d632d6eb5010
_iVyz3r_ <ilhqxmail@gmail.com>
2026-03-08 23:22:21 -0400

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,9 @@
+:: Aegleseeker Client
+:: Copyright (C) 2026 ilyVyzer_
+::
+:: This program is free software: you can redistribute it and/or modify
+:: it under the terms of the GNU General Public License version 3.
+
 @echo off
 echo Setting up the environment...
 echo Installing required packages...
